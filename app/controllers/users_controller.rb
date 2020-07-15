143368278a256405a968fb9e5f735ac893f01fb9
Myriam de Bossoreille <myriam.dbdr@gmail.com>
2020-07-15 09:13:13 +0200

    profile and home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
 
   def dashboard
   end
+
+  def update
+    current_user.update(profil_params)
+    redirect_to root_path
+  end
+
+  private
+  def profil_params
+    params.require(:user).permit(:first_name, :last_name, :phone, :adress )
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,19 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<4;i++) {
+    next=next.next();
+    if (!next.length) {
+      next=$(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
